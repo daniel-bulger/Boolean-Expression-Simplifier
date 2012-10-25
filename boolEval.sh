695696b1c467270839f9b45bbb6288551e7c9185
daniel-bulger <dpbulger@googlemail.com>
2012-10-25 01:09:28 -0400

    Added ability to enter a relative or absolute output file path.  Now actually outputs a .pdf instead of a .vdi

--- boolEval.sh
+++ boolEval.sh
@@ -1,5 +1,5 @@
 INPUT_EXPRESSION=""
-OUTPUTFILE="./Tex Output/output.tex"
+OUTPUTFILE="./output"
 echo $#
 if [ $# -ge 1 ]
     then
@@ -7,15 +7,17 @@ if [ $# -ge 1 ]
     INPUT_EXPRESSION=$1
     if [ $# -eq 2 ]
         then
-        OUTPUTFILE=$2
+        OUTPUTFILE="$2"
     fi
 fi
-
-C++/boolEval $INPUT_EXPRESSION > "$OUTPUTFILE"
+OUTPUTFILE=`basename $OUTPUTFILE ".pdf"`
+C++/boolEval $INPUT_EXPRESSION > "./Tex Output/output.tex"
 if [ $? -eq 0 ]; #normal exit
     then
+    mkdir "Tex Output"
     cd "Tex Output"
-    latex "./output.tex"
-    xdg-open "./output.dvi"
+    latex "-output-format=pdf" "./output.tex" 
     cd ../
+    mv "Tex Output/output.pdf" "$OUTPUTFILE.pdf"
+    xdg-open "$OUTPUTFILE.pdf"
 fi
